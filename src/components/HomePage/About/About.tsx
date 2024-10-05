47e66f56a2495a9d4a38cd18b645dd58a6144472
punjabinuclei <guppi0204@gmail.com>
2024-10-05 15:16:26 +0530

    ResponsiveBeahviorFixed

--- src/components/HomePage/About/About.tsx
+++ src/components/HomePage/About/About.tsx
@@ -6,16 +6,19 @@ import AboutUsText from "../../../assets/AboutUsText.svg"
 
 const About = () => {
     return (
-        <div className=" py-[100px]">
+        <div className=" py-[100px] text-center md:text-left">
             <Container>
-                <div className="flex">
-                    <div className="w-1/2 self-center pt-16">
+                <div className="md:flex">
+                    <div className="md:w-1/2 self-center pt-16">
                         <img src={AboutUsText} />
-                        <h1 className="text-[4rem] tracking-tighter font-medium text-neutral-900 leading-[4rem] pt-6">We help to bring your <br /><span className="italic">dream home </span> to reality</h1>
-                        <p className="text-[1rem] text-[#7C7C7C] tracking-wider  font-light font2 pt-8 w-[92%] leading-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus fringilla dui amet faucibus nam. Erat id laoreet posuere etiam morbi.</p>
-                        <p className="pt-6 text-[1rem] text-[#7C7C7C] tracking-wider  font-light font2 w-[92%] leading-6">Tempor dolor elementum tellus non ipsum faucibus. Justo, magna mauris posuere auctor justo. Habitant proin aliquet volutpat leo ultricies. Dui blandit eget vitae turpis ultrices aliquet nunc. Faucibus sit odio bibendum lobortis diam.</p>
+                        <h1 className="text-[1.8rem] md:text-[4rem] tracking-tighter font-medium text-neutral-900 md:leading-[4rem] pt-6">We help to bring your <br /><span className="italic">dream home </span> to reality</h1>
+                        <div className="md:hidden ">
+                            <img src={AboutUsImg} />
+                        </div>
+                        <p className=" text-[0.9rem] md:text-[1rem] text-[#7C7C7C] tracking-wider  font-light font2 pt-8 mdw-[92%] leading-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus fringilla dui amet faucibus nam. Erat id laoreet posuere etiam morbi.</p>
+                        <p className="pt-6 text-[0.9rem] md:text-[1rem] text-[#7C7C7C] tracking-wider  font-light font2 md:w-[92%] leading-6">Tempor dolor elementum tellus non ipsum faucibus. Justo, magna mauris posuere auctor justo. Habitant proin aliquet volutpat leo ultricies. Dui blandit eget vitae turpis ultrices aliquet nunc. Faucibus sit odio bibendum lobortis diam.</p>
                     </div>
-                    <div className="w-1/2">
+                    <div className="hidden md:block w-1/2">
                         <img src={AboutUsImg} />
                     </div>
                 </div>
--- src/components/HomePage/MetricsBlock/MetricsBlock.tsx
+++ src/components/HomePage/MetricsBlock/MetricsBlock.tsx
@@ -1,26 +1,25 @@
-import Container from "@/components/Common/Container/Container"
-import { clsx } from 'clsx';
+import Container from "@/components/Common/Container/Container";
 
 const Metrics = () => {
     return (
         <Container >
-            <div className="grid grid-cols-3 gap-4 justify-items-center border-b-2 pb-16 border-[#D9D9D6] mb-20 text-[#2C2C2C]">
+            <div className=" grid grid-cols-1    md:grid-cols-3 gap-4 justify-items-center border-b-2 pb-16 border-[#D9D9D6] mb-20 text-[#2C2C2C]">
 
                 <div className="flex">
-                    <h1 className="text-[4.4rem]">100%</h1>
+                    <h1 className="text-[3rem] md:text-[4.4rem]">100%</h1>
                     
-                    <p className="self-center pl-6 uppercase text-[1.1rem] font2">Client <br /> Satisfaction</p>
+                    <p className="self-center pl-2  md:pl-6 uppercase text-[0.7rem] md:text-[1.1rem] font2">Client <br /> Satisfaction</p>
                 </div>
 
                 <div className="flex">
-                    <h1 className="text-[4.4rem]">250</h1>
-                    <p className="self-center pl-6 uppercase text-[1.1rem] font2">Employees On<br />Worldwide </p>
+                    <h1 className="text-[3rem] md:text-[4.4rem]">250+</h1>
+                    <p className="self-center pl-3 md:pl-6 uppercase text-[0.7rem]  md:text-[1.1rem] font2">Employees On<br />Worldwide </p>
                 </div>
 
 
                 <div className="flex">
-                    <h1 className="text-[4.4rem]">3469</h1>
-                    <p className="self-center pl-6 uppercase text-[1.1rem] font2">Projects Completed <br /> in 60 Countries</p>
+                    <h1 className= "text-[3rem] md:text-[4.4rem]">3469+</h1>
+                    <p className="self-center pl-3 md:pl-4 uppercase text-[0.7rem] md:text-[1rem] font2">Projects Completed <br /> in 60 Countries</p>
                 </div>
 
             </div>
