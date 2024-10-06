3b9e367e2b3b7de6f0ac3ff72d2c877c54a2e33d
punjabinuclei <guppi0204@gmail.com>
2024-10-06 15:00:46 +0530

    UI_FIX

--- src/components/Common/Navbar/Navbar.tsx
+++ src/components/Common/Navbar/Navbar.tsx
@@ -1,20 +1,21 @@
 import Container from "@/components/Common/Container/Container"
 import { Link } from "react-router-dom"
+import "./style.css"
 
 const Navbar = () => {
     return (
         <Container>
-            <div className="flex justify-between md:justify-between pt-8 ">
+            <div className="flex justify-between md:justify-between pt-4 ">
                 <div className="md:flex">
-                    <h1 className="md:text-[2rem] relative top-1 md:bottom-4"><span className="text-[#996830]">MOHIT</span> DANGI</h1>
+                    <h1 className="md:text-[2rem] relative top-1  md:bottom-6"><span className="text-[#996830]">MOHIT</span> DANGI</h1>
                 </div>
-                <div className="hidden md:flex justify-between font2 tracking-tight text-[0.9rem] font-semibold">
-                    <Link to="HomePage"><p className="px-6">Home</p></Link>
-                    <Link to="About"><p className="px-6">About</p></Link>
-                    <Link to="Services"><p className="px-6">Services</p></Link>
-                    <Link to="OurWork"><p className="px-6">Our Work</p></Link>
+                <div className="hidden md:flex justify-between font2 tracking-wide text-[0.9rem] relative top-6">
+                    <Link to="HomePage"><p className="px-6 hover-underline-animation">Home</p></Link>
+                    <Link to="About"><p className="px-6 hover-underline-animation">About</p></Link>
+                    <Link to="Services"><p className="px-6 hover-underline-animation">Services</p></Link>
+                    <Link to="OurWork"><p className="px-6 hover-underline-animation">Our Work</p></Link>
                     <div>
-                        <button className="relative bottom-2 ml-6 inline-flex bg-[#996830] px-6 py-2 text-white rounded-md box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;">Contact us <svg xmlns="http://www.w3.org/2000/svg" width="1.6em" height="1.6em" viewBox="0 0 24 24">
+                        <button className="relative bottom-2 ml-6 inline-flex bg-[#996830] px-6 py-2 text-white rounded-md box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px; ">Contact us <svg xmlns="http://www.w3.org/2000/svg" width="1.6em" height="1.6em" viewBox="0 0 24 24">
                             <path fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" d="M7 7h10m0 0v10m0-10L7 17" />
                         </svg></button>
                     </div>
--- src/components/HomePage/RecentWorks/RecentWorks.tsx
+++ src/components/HomePage/RecentWorks/RecentWorks.tsx
@@ -13,14 +13,14 @@ const RecentWorks = () => {
                 <div>
                     <img src={RecentWorkText} />
                 </div>
-                <h1 className="text-[1.7rem] md:text-[4rem] tracking-tighter font-medium text-neutral-900 md:leading-[4rem] pt-6">Some of<span className="italic"> our crafts</span> <br />made with love to reality</h1>
+                <h1 className="text-[1.7rem] md:text-[4rem] tracking-tighter font-medium text-neutral-900 md:leading-[4rem] pt-6 ">Some of<span className="italic"> our crafts</span> <br />made with love to reality</h1>
 
                 <div className="md:grid grid-cols-2 gap-20 justify-items-center">
                     <div className="pt-[46px] md:pt-[96px]">
                         <img src={Works1} />
 
-                        <h1 className= "text-[1.5rem] md:text-[2.5rem] tracking-tighter  text-neutral-900 md:leading-[4rem] pt-6">Villa Furnishing & Interior</h1>
-                        <p className="text-[0.8rem] text-neutral-600 font2 tracking-widest">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus fringilla dui amet faucibus nam. Erat id laoreet posuere etiam morbi.</p>
+                        <h1 className= "text-[1.5rem] md:text-[2.5rem] tracking-tighter  text-neutral-900 md:leading-[4rem] pt-6 ">Villa Furnishing & Interior</h1>
+                        <p className="text-[0.8rem] text-neutral-600 font2 md:text-[1.1rem] tracking-widest pb-8 md:pb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus fringilla dui amet faucibus nam. Erat id laoreet posuere etiam morbi.</p>
 
                         <div className="mt-16 hidden md:block pt-4 md:pt-0">
                             <button className="inline-flex bg-[#996830] px-6 py-[1rem] text-white font2 uppercase text-[0.9rem]">View More <svg xmlns="http://www.w3.org/2000/svg" width="1.6em" height="1.6em" viewBox="0 0 24 24">
@@ -32,12 +32,12 @@ const RecentWorks = () => {
                         <div className="relative md:bottom-28" >
                             <img src={Works2} />
                             <h1 className="text-[1.5rem] md:text-[2.5rem] tracking-tighter  text-neutral-900 leading-[4rem] pt-6">Luxury Hotel Renovation</h1>
-                            <p className="text-neutral-600 text-[0.8rem] font2 tracking-widest">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus fringilla dui amet faucibus nam.</p>
+                            <p className="text-neutral-600 text-[0.8rem] md:text-[1.1rem] font2 tracking-widest pb-8 md:pb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus fringilla dui amet faucibus nam.</p>
                         </div>
                         <div className="relative md:bottom-16">
                             <img src={Works3} />
                             <h1 className="text-[1.5rem] md:text-[2.5rem] tracking-tighter  text-neutral-900 leading-[4rem] pt-6">Residence Swimming Pool</h1>
-                            <p className="text-[0.8rem]  text-neutral-600 font2 tracking-widest">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus fringilla dui amet faucibus nam.</p>
+                            <p className="text-[0.8rem]  text-neutral-600 md:text-[1.1rem] font2 tracking-widest pb-8 md:pb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus fringilla dui amet faucibus nam.</p>
                         </div>
                     </div>
                 </div>
